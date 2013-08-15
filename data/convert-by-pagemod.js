7a9bff8f7e37df78877401ec8b3ae3b70571f913
Brett Zamir <brettz9@yahoo.com>
2013-08-15 08:05:57 +0800

    indent

--- data/convert-by-pagemod.js
+++ data/convert-by-pagemod.js
@@ -1,7 +1,7 @@
 /*globals self, cmConvertClick*/
 
 (function () {
-'use strict';
+    'use strict';
     self.port.on('passOptions', function(opts) {
         // We use this condition to avoid showing this (whole-page) context menu on pages like Github's non-raw (text/html) view of a coffee file
         if (['text/plain', 'text/coffeescript'].indexOf(document.contentType) > -1) {
